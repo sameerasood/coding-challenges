ebdcda41cbb247f738e0d8b7a90cc48090c5a2ba
sameera sood <sameerasood@gmail.com>
2023-06-10 21:57:12 +0100

    test passed for 4

--- primeNumber.js
+++ primeNumber.js
@@ -4,6 +4,12 @@ class PrimeNumber {
   }
 
   getResult() {
+    for (let i = 2; i <= Math.sqrt(this.num); i++) {
+      if (this.num % i == 0) {
+        return `${this.num} is not a prime number.`;
+      }
+    }
+
     return `${this.num} is a prime number.`;
   }
 }
