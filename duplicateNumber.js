5fb9177fee1b33ee7458ba8c6ba70d7e248c2155
sameera sood <sameerasood@gmail.com>
2023-06-12 11:42:56 +0100

    test passed for 2 elements

--- duplicateNumber.js
+++ duplicateNumber.js
@@ -1,11 +1,18 @@
-class DuplicateNumber {
+class RemoveDuplicates {
   constructor(arr) {
     this.arr = arr;
+    this.newArr = [];
   }
 
   getResult() {
-    return this.arr;
+    for (let i = 0; i < this.arr.length; i++) {
+      const currentValue = this.arr[i];
+      if (!this.newArr.includes(currentValue)) {
+        this.newArr.push(currentValue);
+      }
+    }
+    return this.newArr;
   }
 }
 
-module.exports = DuplicateNumber;
+module.exports = RemoveDuplicates;
--- duplicateNumber.test.js
+++ duplicateNumber.test.js
@@ -1,8 +1,13 @@
-const DuplicateNumber = require("./duplicateNumber.js");
+const RemoveDuplicates = require("./duplicateNumber.js");
 
 describe("class DuplicateNumber", () => {
   it("returns the array unchanged", () => {
-    const duplicateNumber = new DuplicateNumber([1]);
+    const duplicateNumber = new RemoveDuplicates([1]);
+    expect(duplicateNumber.getResult()).toEqual([1]);
+  });
+
+  it("removes duplicate from 2 elements array", () => {
+    const duplicateNumber = new RemoveDuplicates([1, 1]);
     expect(duplicateNumber.getResult()).toEqual([1]);
   });
 });
